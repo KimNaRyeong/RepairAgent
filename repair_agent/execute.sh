adebe4be51f64f2097a00ac6d11ca4251d812e91
KimNaRyeong <kimnal1234@kaist.ac.kr>
2025-07-04 08:47:48 +0000

    closure portion

--- repair_agent/execute.sh
+++ repair_agent/execute.sh
@@ -1,5 +1,5 @@
-./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
-./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
+# ./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
+# ./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
 ./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
 ./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
 ./run_on_defects4j.sh experimental_setups/bugs_list_closure hyperparams.json
