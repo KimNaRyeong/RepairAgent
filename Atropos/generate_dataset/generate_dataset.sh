5e28aa3b7236d8e7774fd1f0d4cc898567a796a9
KimNaRyeong <kimnal1234@kaist.ac.kr>
2025-08-21 02:10:07 +0000

    categorize results

--- Atropos/generate_dataset/generate_dataset.sh
+++ Atropos/generate_dataset/generate_dataset.sh
@@ -9,13 +9,13 @@
 # python raw_response_fasttext_clustering.py -l 9 -t 0.96 -m 0.97
 # python raw_response_fasttext_clustering.py -l 10 -t 0.96 -m 0.97
 
-python only_action_one_hot.py -l 1 -a 39
-python only_action_one_hot.py -l 2 -a 39
-python only_action_one_hot.py -l 3 -a 39
-python only_action_one_hot.py -l 4 -a 39
-python only_action_one_hot.py -l 5 -a 39
-python only_action_one_hot.py -l 6 -a 39
-python only_action_one_hot.py -l 7 -a 39
-python only_action_one_hot.py -l 8 -a 39
-python only_action_one_hot.py -l 9 -a 39
-python only_action_one_hot.py -l 10 -a 39
+python only_action_categorize.py -l 1
+python only_action_categorize.py -l 2
+python only_action_categorize.py -l 3
+python only_action_categorize.py -l 4
+python only_action_categorize.py -l 5
+python only_action_categorize.py -l 6
+python only_action_categorize.py -l 7
+python only_action_categorize.py -l 8
+python only_action_categorize.py -l 9
+python only_action_categorize.py -l 10
--- Atropos/model/run_model_only_action.sh
+++ Atropos/model/run_model_only_action.sh
@@ -1,13 +1,13 @@
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_1
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_2
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_3
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_4
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_5
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_6
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_7
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_8
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_9
-python gcn_model.py -d ../data/only_action/40/processed_response/label_criteria_10
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_1
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_2
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_3
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_4
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_5
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_6
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_7
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_8
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_9
+python gcn_model.py -d ../data/only_action/categorize/processed_response/label_criteria_10
 
 # python gcn_model.py -d ../data/clustering/fasttext/sentence_vector/100/raw_response/0.98_0.99/label_criteria_1
 # python gcn_model.py -d ../data/clustering/fasttext/sentence_vector/100/raw_response/0.98_0.99/label_criteria_3
