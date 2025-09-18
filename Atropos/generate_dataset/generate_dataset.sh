73d0541d3680a260cca1304bb525f3ca51d67437
KimNaRyeong <kimnal1234@kaist.ac.kr>
2025-09-18 11:57:15 +0000

    training with various condition

--- Atropos/generate_dataset/generate_dataset.sh
+++ Atropos/generate_dataset/generate_dataset.sh
@@ -1,24 +1,24 @@
-python raw_response_fasttext_clustering.py -l 1 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 2 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 3 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 4 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 5 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 6 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 7 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 8 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 9 -t 0.95 -m 0.96 -wp
-python raw_response_fasttext_clustering.py -l 10 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 1 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 2 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 3 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 4 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 5 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 6 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 7 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 8 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 9 -t 0.95 -m 0.96 -wp
+# python raw_response_fasttext_clustering.py -l 10 -t 0.95 -m 0.96 -wp
 
-python raw_response_fasttext_clustering.py -l 1 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 2 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 3 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 4 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 5 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 6 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 7 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 8 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 9 -t 0.99 -m 0.99 -wp
-python raw_response_fasttext_clustering.py -l 10 -t 0.99 -m 0.99 -wp9
+# python raw_response_fasttext_clustering.py -l 1 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 2 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 3 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 4 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 5 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 6 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 7 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 8 -t 0.99 -m 0.99 -wp
+# python raw_response_fasttext_clustering.py -l 9 -t 0.99 -m 0.99 -wp
+python raw_response_fasttext_clustering.py -l 10 -t 0.99 -m 0.99 -wp
 # python only_action_categorize.py -l 1 -p
 # python only_action_categorize.py -l 2 -p
 # python only_action_categorize.py -l 3 -p
